8079817872daac848b257f9745499e5d7803b329
ma7ma7 <mahmoudamane5@gmail.comm>
2022-01-12 22:50:28 +0100

    Create the Product Component

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,31 @@
 class ProductList extends React.Component {
   render() {
     return (
-      <div className='ui unstackable items'>Hello from Product List 👋</div>
+      <div className='ui unstackable items'>
+        <Product />
+      </div>
+    );
+  }
+}
+
+class Product extends React.Component {
+  render() {
+    return (
+      <div className='item'>
+        <div className='image'>
+          <img src='images/products/image-aqua.png' alt='Aqua' />
+        </div>
+        <div className='middle aligned content'>
+          <div className='description'>
+            <a href=''>Fort Knight</a>
+            <p>Authentic renaissance actors, delivered in just two weeks</p>
+          </div>
+          <div className='extra'>
+            <span>Submited By:</span>
+            <img src='images/avatars/daniel.jpg' className='ui avatar image' />
+          </div>
+        </div>
+      </div>
     );
   }
 }
