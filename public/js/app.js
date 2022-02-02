a909d0a58dc6ed93e9717a2b36d2fd37acaeb0b4
ma7ma7 <mahmoudamane5@gmail.comm>
2022-02-03 00:24:01 +0100

    Handle upvote method

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,36 @@
 class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: [],
+    };
+
+    this.handleProductUpVote = this.handleProductUpVote.bind(this);
+  }
+
   handleProductUpVote(productId) {
-    console.log(productId + ' was upvoted.');
+    const nextProducts = this.state.products.map((product) => {
+      if (product.id === productId) {
+        return Object.assign({}, product, {
+          votes: product.votes + 1,
+        });
+      } else {
+        return product;
+      }
+    });
+
+    this.setState({
+      products: nextProducts,
+    });
+  }
+
+  componentDidMount() {
+    this.setState({ products: Seed.products });
   }
 
   render() {
-    const products = Seed.products.sort((a, b) => b.votes - a.votes);
+    const products = this.state.products.sort((a, b) => b.votes - a.votes);
     const productComponents = products.map((product) => (
       <Product
         key={'product-' + product.id}
